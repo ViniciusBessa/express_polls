08cd342971bd9df47383ebf03516b3219934228d
Vinicius <vinicius.b.silva3@gmail.com>
2022-03-01 16:23:59 -0300

    Sistema de cadastro e login de usuários foi criado

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,27 @@
 const asyncWrapper = require('../middlewares/async-wrapper');
+const Message = require('../middlewares/message');
+const db = require('../db/db');
+const crypto = require('crypto');
 
 const getPaginaCadastro = asyncWrapper(async (req, res) => {
   res.status(200).render('cadastro');
 });
 
 const cadastrarUsuario = asyncWrapper(async (req, res) => {
-  
+  const { username, password, email } = req.body;
+  const { session } = req;
+
+  if (!username || !password || !email || username.length === 0 || password.length === 0 || email.length === 0) {
+    const message = new Message('Preencha todos os campos', 'error');
+    return res.status(400).render('cadastro', { message });
+  }
+
+  const salt = crypto.randomBytes(16).toString('hex');
+  let hashedPassword = crypto.scryptSync(password, salt, 64).toString('hex');
+  hashedPassword = `${hashedPassword}:${salt}`;
+  const [userData] = await db('users').insert({ username, password: hashedPassword, email }).returning('id');
+  session.userID = userData.id;
+  res.status(201).redirect('/');
 });
 
 const getPaginaLogin = asyncWrapper(async (req, res) => {
@@ -13,7 +29,31 @@ const getPaginaLogin = asyncWrapper(async (req, res) => {
 });
 
 const logarUsuario = asyncWrapper(async (req, res) => {
+  const { username, password } = req.body;
+  const { session } = req;
+
+  if (!username || !password || username.length === 0 || password.length === 0) {
+    const message = new Message('Preencha todos os campos', 'error');
+    return res.status(400).render('login', { message });
+  }
+  let [userData] = await db('users').where({ username }).select('id', 'password');
   
+  if (!userData) {
+    const message = new Message('Usuário não encontrado', 'error');
+    return res.status(400).render('login', { message });
+  }
+  const [userKey, salt] = userData.password.split(':');
+  const hashedBuffer = crypto.scryptSync(password, salt, 64);
+  const hashedKey = Buffer.from(userKey, 'hex');
+
+  const passwordMatch = crypto.timingSafeEqual(hashedBuffer, hashedKey);
+
+  if (!passwordMatch) {
+    const message = new Message('Senha inserida está incorreta', 'error');
+    return res.status(400).render('login', { message });
+  }
+  session.userID = userData.id;
+  res.status(200).redirect('/');
 });
 
 module.exports = {
--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(500).send('<h1>Ocorreu um erro no servidor, tente mais tarde.</h1>');
+  res.status(500).send('<h1>Ocorreu um erro no servidor, tente novamente mais tarde.</h1>');
 };
 
 module.exports = errorHandler;
